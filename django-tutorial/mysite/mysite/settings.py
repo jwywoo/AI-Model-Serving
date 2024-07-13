c7519d1d507f51b9f137819647492bbdae5f6ba0
jwywoo <jwywoo26@gmail.com>
2024-07-13 17:10:29 +0900

    [FEAT] #1-2 Django Views, Urls, Models

--- django-tutorial/mysite/mysite/settings.py
+++ django-tutorial/mysite/mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    ## Created app
+    "polls.apps.PollsConfig",
 ]
 
 MIDDLEWARE = [
