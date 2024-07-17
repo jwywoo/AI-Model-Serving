b835aa29dac98e2891fdacc903dddac6d158ae1f
jwywoo <jwywoo26@gmail.com>
2024-07-17 23:13:08 +0900

    [FEAT] #8-2 FastAPI Practice
    Split main.py into different modules

---
+++ fastapi-modules/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from .routers import post
+
+app = FastAPI()
+
+app.include_router(post.router)
+
+@app.get("/")
+async def root():
+    return {"message": "FastAPI app -> better structures"}
