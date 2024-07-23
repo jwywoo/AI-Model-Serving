300644105b6dc2d5e3f2aaf858b162ab10ad2c6c
jwywoo <jwywoo26@gmail.com>
2024-07-23 22:25:54 +0900

    [FEAT] #15 Model Serving Hotfix
    Done!
    - need enhancement related to error handlings
    - requests

--- model-serving-test/app/crud/prediction_crud.py
+++ model-serving-test/app/crud/prediction_crud.py
@@ -8,17 +8,29 @@ from ..core.config import settings
 from ..enum.obs_enum import ObsEnum
 
 def get_prediction(request):
+    # find obs
     selected_obs = find_obs(request=request)
+    # update df to recent data
     df_up_to_date = weather_recent(obs=selected_obs)
+    print("here? 1")
+    if (df_up_to_date is None):
+        return {
+            "message": "Errorrrrrr"
+        }
+    # Preprocessing
     num_cols = df_up_to_date.drop(columns=['baseDate']).columns
     temp_df = df_up_to_date[num_cols].apply(pd.to_numeric, errors='coerce')
     df_interpolated = temp_df.interpolate(method='linear', axis=0)
     df_interpolated['baseDate'] = df_up_to_date['baseDate']
     preprocessed_data = data_preprocessing(df_interpolated)
+
+    # Prediction
     last_row = preprocessed_data.iloc[-1]
     last_row_df = last_row.to_frame().T.drop(columns=["baseDate", "dailyRainfall"])
     model = joblib.load(selected_obs['path_to_model'])
     model_prediction = model.predict(last_row_df)[0]
+
+
     if (model_prediction == 1):
         return {
             "location": selected_obs['observatoryName'],
@@ -33,6 +45,7 @@ def get_prediction(request):
 # Supporting Methods
 # finding observatory based on given request
 def find_obs(request):
+    print("find_obs")
     for obs in ObsEnum:
         obs_info = obs.value
         obs_long = float(obs_info['longitude'])
@@ -43,10 +56,11 @@ def find_obs(request):
 
 # Updating recent weather data
 def weather_recent(obs):
+    print("weather_recent")
     obs_name = obs['observatoryName']
     obs_last_update = obs['last_update']
     now = datetime.now().date()
-    df_up_to_date = pd.DataFrame(columns=obs['exclude'])
+    df_up_to_date = pd.DataFrame(columns=obs['include'])
     while (obs_last_update < now):
         project_key = settings.project_key
         year_month_day = obs_last_update.strftime("%Y%m%d")
@@ -60,6 +74,8 @@ def weather_recent(obs):
             except ValueError as e:
                 print(f"Error decoding JSON for {year_month_day}: {e}")
                 print(response.text)
+        else:
+            return None
         obs_last_update+=timedelta(days=1)
     return df_up_to_date
 
@@ -69,18 +85,19 @@ def json_parsing_200(data, obs, base_date):
     if (len(given_data) != 0):
         given_data = given_data[0]
         df = pd.DataFrame()
-        for column in obs['exclude']:
+        for column in obs['include']:
             df[column] = [given_data.get(column)]
         return df
     else:
         df = pd.DataFrame()
-        for column in obs['exclude']:
+        for column in obs['include']:
             df[column] = [np.nan]
         df['baseDate'] = [base_date]
         return df
 
 # Preprocessing
 def data_preprocessing(df):
+    print("preprocessing")
     data = df.sort_values('baseDate')
     prev_day_1 = data.shift(1).drop(columns=['baseDate'])
     prev_day_2 = data.shift(2).drop(columns=['baseDate'])
@@ -90,4 +107,5 @@ def data_preprocessing(df):
     combined_data = pd.concat([data['baseDate'], data['dailyRainfall'],prev_day_1.add_suffix('_prev1'), prev_day_2.add_suffix('_prev2'), prev_day_3.add_suffix('_prev3')], axis=1)
     combined_data = combined_data.dropna()
     combined_data = combined_data.reset_index(drop=True)
+
     return combined_data
--- model-serving-test/app/enum/obs_enum.py
+++ model-serving-test/app/enum/obs_enum.py
@@ -2,18 +2,18 @@ from enum import Enum
 from datetime import date
 
 # 대정, 구좌, 영실, 서광, 진달래밭, 새별오름, 애월, 외도
-columns_1 = ['observatoryName', 'lowestTemperatureTime', 'highestTemperatureTime', 'maximumWindSpeedTime', 'maximumWindSpeedDirection']
+columns_1 = ['baseDate', 'averageTemperature', 'lowestTemperature', 'highestTemperature', 'dailyRainfall', 'maximumWindSpeed', 'averageWindSpeed']
 
 # 제주남원, 제주가시리, 중문
-columns_2 = ['observatoryName', 'lowestTemperatureTime', 'highestTemperature', 'highestTemperatureTime', 'maximumWindSpeedTime', 'maximumWindSpeedDirection', 'averageWindSpeed']
+columns_2 = ['baseDate', 'averageTemperature', 'lowestTemperature', 'dailyRainfall', 'maximumWindSpeed']
 
 # 대흘 
-columns_3 = ['observatoryName', 'lowestTemperatureTime', 'highestTemperature', 'highestTemperatureTime', 'maximumWindSpeedTime', 'maximumWindSpeedDirection']
+columns_3 = ['baseDate', 'averageTemperature', 'lowestTemperature', 'dailyRainfall', 'maximumWindSpeed', 'averageWindSpeed']
 
 #어리목
-columns_4 = ['observatoryName', 'lowestTemperatureTime', 'highestTemperatureTime', 'maximumWindSpeedTime']
+columns_4 = ['baseDate', 'averageTemperature', 'lowestTemperature', 'highestTemperature', 'dailyRainfall', 'maximumWindSpeed', 'maximumWindSpeedDirection', 'averageWindSpeed']
 
-# 월정, 마라도
+# 월정, 마라도 -> done
 columns_5 = ['baseDate', 'averageTemperature', 'lowestTemperature', 'highestTemperature', 'dailyRainfall', 'maximumWindSpeed']
 
 class ObsEnum(Enum):
@@ -23,7 +23,7 @@ class ObsEnum(Enum):
 		"longitude": "126.2679",
 		"latitude": "33.1221",
         "last_update": date(2024,7,2),
-        "exclude": columns_5,
+        "include": columns_5,
         "path_to_model":"app/trained_models/마라도_esm_model.pkl",
     }
     # 외도
@@ -31,124 +31,124 @@ class ObsEnum(Enum):
         "observatoryName": "외도",
 		"longitude": "126.4317",
 		"latitude": "33.4769",
-        "exclude":columns_1,
+        "include":columns_1,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/외도_esm_model.pkl",
     }
     # 대정
     obs_793 = {
         "observatoryName": "대정",	
 		"longitude": "126.2263",
 		"latitude": "33.2410",
-        "exclude":columns_1,
+        "include":columns_1,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/대정_esm_model.pkl",
     }
     # 중문
     obs_328 = {
 		"observatoryName": "중문",
 		"longitude": "126.4060",
 		"latitude": "33.2494",
-        "exclude":columns_2,
+        "include":columns_2,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/중문_esm_model.pkl",
     }
     # 제주남원
     obs_780 = {
         "observatoryName": "제주남원",
 		"longitude": "126.7044",
 		"latitude": "33.2772",
-        "exclude":columns_2,
+        "include":columns_2,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/제주남원_esm_model.pkl",
     }
     # 대흘
     obs_330 = {
 		"observatoryName": "대흘",
 		"longitude": "126.6495",
 		"latitude": "33.5008",
-        "exclude":columns_3,
+        "include":columns_3,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/대흘_esm_model.pkl",
     }
     # 구좌
     obs_781 = {
 		"observatoryName": "구좌",
 		"longitude": "126.8777",
 		"latitude": "33.5199",
-        "exclude":columns_1,
+        "include":columns_1,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/구좌_esm_model.pkl",
     }
     # 진달래밭
     obs_870 = {
 		"observatoryName": "진달래밭",
 		"longitude": "126.5557",
 		"latitude": "33.3698",
-        "exclude":columns_1,
+        "include":columns_1,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/진달래밭_esm_model.pkl",
     }
     # 영실
     obs_869 = {
         "observatoryName": "영실",
 		"longitude": "126.4964",
 		"latitude": "33.3483",
-        "exclude":columns_1,
+        "include":columns_1,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/영실_esm_model.pkl",
     }
     # 서광
     obs_752 = {
 		"observatoryName": "서광",	
 		"longitude": "126.3060",
 		"latitude": "33.3046",
-        "exclude":columns_1,
+        "include":columns_1,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/서광_esm_model.pkl",
     }
     # 새별오름
     obs_883 = {
         "observatoryName": "새별오름",
 		"longitude": "126.3599",
 		"latitude": "33.3623",
-        "exclude":columns_1,
+        "include":columns_1,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/새별오름_esm_model.pkl",
     }
     # 어리목
     obs_753 = {
 		"observatoryName": "어리목",
 		"longitude": "126.4959",
 		"latitude": "33.3930",
-        "exclude":columns_4,
+        "include":columns_4,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/어리목_esm_model.pkl",
     }
     # 월정
     obs_861 = {
 		"observatoryName": "월정",
 		"longitude": "126.7781",
 		"latitude": "33.5623",
-		"exclude": columns_5,
+		"include": columns_5,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/월정_esm_model.pkl",
     }
     # 제주가시리
     obs_890 = {
 		"observatoryName": "제주가시리",
 		"longitude": "126.7336",
 		"latitude": "33.3854",
-        "exclude":columns_2,
+        "include":columns_2,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/제주가시리_esm_model.pkl",
     }
     # 애월
     obs_893 = {
 		"observatoryName": "애월",
 		"longitude": "126.3275",
 		"latitude": "33.4659",
-        "exclude":columns_1,
+        "include":columns_1,
         "last_update": date(2024,7,2),
-        "path_to_model":"",
+        "path_to_model":"app/trained_models/애월_esm_model.pkl",
     }
