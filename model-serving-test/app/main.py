d891bb32649585a50dd9817d6945fc332c58740d
jwywoo <jwywoo26@gmail.com>
2024-07-27 17:02:44 +0900

    [FEAT] #29-2 Optimization Error: 500

--- model-serving-test/app/main.py
+++ model-serving-test/app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
 from .routers import prediction_router
 from .core.config import settings
 from fastapi.middleware.cors import CORSMiddleware
@@ -7,6 +9,14 @@ app = FastAPI()
 
 # Router
 app.include_router(prediction_router.router)
+
+# Exception
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"message": "Please try again later."},
+    )
 
 # Cors
 origins = [
